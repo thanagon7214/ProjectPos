2c21c5eafeb9e9db09f9c14123e2e8e5c8aef2ae
Countboss <108651491+thanagon7214@users.noreply.github.com>
2024-11-15 16:35:57 +0700

    update file

---
+++ ProjectPos/Controllers/FoodFactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectPos.Controllers
+{
+    public class FoodFactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FoodFactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public async Task<IActionResult> Index() {
+            return View();
+        }
+    }
+}
--- ProjectPos/Program.cs
+++ ProjectPos/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//register HttpClient
+builder.Services.AddHttpClient();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
